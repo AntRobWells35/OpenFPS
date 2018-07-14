fc23b2ab39200ed344a49b7d8793a842d9914f49
AntRobWells35 <40861032+AntRobWells35@users.noreply.github.com>
2018-07-14 23:25:46 +0100

    Titles Editor and implentation working

--- Assets/Code/Presentation/Titles/TitlesEditor.cs
+++ Assets/Code/Presentation/Titles/TitlesEditor.cs
@@ -5,7 +5,8 @@ using System.Collections;
 
 public class TitlesEditor : EditorWindow {
 
-    public AudioClip TitleTrack;
+    Titles TitlesObj;
+    Texture curTex;
 
     [MenuItem("OpenFPS/Titles Editor")]
     public static void ShowWindow()
@@ -16,12 +17,54 @@ public class TitlesEditor : EditorWindow {
     void OnGUI()
     {
 
-        GUILayout.BeginHorizontal();
-        GUILayout.Label("Title Track", EditorStyles.boldLabel);
-        TitleTrack = EditorGUILayout.ObjectField(TitleTrack,typeof(AudioClip),true) as AudioClip;
+        GUILayout.BeginVertical();
+        GUILayout.BeginHorizontal(); 
+
+        GUILayout.Label("Titles Obj", EditorStyles.boldLabel);
+        TitlesObj = EditorGUILayout.ObjectField(TitlesObj, typeof(Titles), true) as Titles;
+        if (GUILayout.Button("New"))
+        {
+            TitlesObj = GameObject.Instantiate<Titles>(GameGlobals.StatGlobal.TitlesBase);
+            TitlesObj.name = "NewTitles1";
+            TitlesObj.Use = true;
+          //TitlesObj = GameGlobals.StatGlobal.TitlesBase.gameObject.   
+
+        }
         GUILayout.EndHorizontal();
 
-        
+        if (TitlesObj == null) return;
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Title Track", EditorStyles.boldLabel);
+        TitlesObj.TitleTrack = EditorGUILayout.ObjectField(TitlesObj.TitleTrack,typeof(AudioClip),true) as AudioClip;
+      
+        GUILayout.EndHorizontal();
+
+        GUILayout.Label("Titles");
+
+        GUILayout.BeginVertical();
+
+        int ti = 1;
+        foreach(var tex in TitlesObj.Logos)
+        {
+            GUILayout.Label("Title:" + ti.ToString() + ":" + tex.name);
+            ti++;
+        }
+
+        GUILayout.EndVertical();
+
+        GUILayout.BeginHorizontal();
+        curTex = EditorGUILayout.ObjectField(curTex, typeof(Texture), true) as Texture;
+        if (GUILayout.Button("New Title"))
+        {
+            if (curTex != null)
+            {
+                TitlesObj.Logos.Add(curTex);
+            }
+        }
+        GUILayout.EndHorizontal();
+
+        GUILayout.EndVertical();
     }
 
 }
