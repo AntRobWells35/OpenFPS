e59e6dfaea36a37be29e82b04521d882f6bcce72
AntRobWells35 <40861032+AntRobWells35@users.noreply.github.com>
2018-07-14 22:02:49 +0100

    basic work begun

---
+++ Assets/Code/Presentation/Titles/TitlesEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+
+public class TitlesEditor : EditorWindow {
+
+    public AudioClip TitleTrack;
+
+    [MenuItem("OpenFPS/Titles Editor")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(TitlesEditor));
+    }
+
+    void OnGUI()
+    {
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Title Track", EditorStyles.boldLabel);
+        TitleTrack = EditorGUILayout.ObjectField(TitleTrack,typeof(AudioClip),true) as AudioClip;
+        GUILayout.EndHorizontal();
+
+        
+    }
+
+}
