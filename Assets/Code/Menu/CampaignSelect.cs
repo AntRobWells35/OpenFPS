9e066a68058e847ed035631fc529329b2f2fa02e
Antony R Wells <antonyrwells@outlook.com>
2018-07-16 09:37:34 +0100

    progress

---
+++ Assets/Code/Menu/CampaignSelect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CampaignSelect : MonoBehaviour {
+
+    List<CampaignInfo> Cams;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    void OnGUI(){
+
+        GUI.Window(0, new Rect(50, 100, 450, 600), CamWin, "Choose Campaign");
+
+    }
+
+    void CamWin(int win){
+        
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
